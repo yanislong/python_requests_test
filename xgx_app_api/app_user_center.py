54a6d0f2fcee097fc7b6aa98bd72ae88bb802cf0
yanislong <335916781@qq.com>
2017-05-22 08:42:56 +0100

    update modify

--- xgx_app_api/app_user_center.py
+++ xgx_app_api/app_user_center.py
@@ -2,13 +2,100 @@
 # -*-coding=utf-8 -*-
 
 import requests
+import app_user_register
+import re
+import hashlib
 
-def user_information():
-    url = "http://cuimingwen.api.xueguoxuewang.cn/v1/user/center"
+
+def pw_md5(pw):
+    mima = hashlib.md5()
+    mima.update(pw)
+    return mima.hexdigest()
+
+
+def user_information(myphone):
+    url = "https://cuimingwen.api.xueguoxuewang.cn/v1/user/center"
     header = {}
     data = {}
-    r = requests.get(url)
+    data['token'] = app_user_register.user_login(myphone)
+    r = requests.get(url, params=data, verify=False)
+    print r.url
+    print r.content
+
+def send_code(myphone):
+    url = "http://cuimingwen.api.xueguoxuewang.cn/v1/user/passwd/msg/send"
+    data = {'phone':myphone}
+    r = requests.post(url, data=data)
+    l1 = re.compile('\d{6}')
+    l2 = l1.findall(r.content)
+    try:
+        if l2:
+            print "短信发送成功，验证码： " + l2[0]
+            return l2[0]
+        else:  
+            print r.content
+    except:
+        print 'ifcate error'
+        return None
+
+def reset_passwd(myphone):
+    url = "http://cuimingwen.api.xueguoxuewang.cn/v1/user/passwd/reset"
+    data = {}
+    data['phone'] = myphone
+    data['code'] = "464878"#send_code(myphone)
+    data['password'] = pw_md5('123456789')
+    r = requests.post(url, data=data)
+    print r.content
+
+def user_identity():
+    url = "http://cuimingwen.api.xueguoxuewang.cn/v1/user/identity-list"
+    data = {'token':app_user_register.user_login('13166666249')}
+    r = requests.get(url, params=data)
+    print r.content
+
+def user_identity_save(myphone):
+    url = "http://cuimingwen.api.xueguoxuewang.cn/v1/user/identity"
+    data = {}
+    data['identity'] = (40,60,30)
+    data['interests[]'] = ((103,105),)
+    param = {'token':app_user_register.user_login(myphone)}
+    r = requests.post(url, data=data, params=param)
+    print r.url
+    print r.content
+    
+def user_collect(myphone):
+    url = "http://cuimingwen.api.xueguoxuewang.cn/v1/user/center/collection"
+    header = {}
+    param = {}
+    param['type'] = "10"
+    param['token'] = app_user_register.user_login(myphone)
+    r = requests.get(url, params=param)
+    print r.content
+   
+def user_order(myphone):
+    url = "http://cuimingwen.api.xueguoxuewang.cn/v1/user/center/order"
+    param = {}
+    param['order_status'] = 10
+    param['page'] = 1
+    param['token'] = app_user_register.user_login(myphone)
+    r = requests.get(url, params=param)
+    print r.content
+
+def order_information(myphone):
+    url = ""
+    param = {}
+    param['token'] = app_user_register.user_login(myphone)
+    param['order_code'] = ""
+    r = requests.get(url, params=param)
     print r.content
 
 if __name__ == "__main__":
-    user_information()
+    myphone = "13166666241"
+    user_information(myphone)
+#    send_code(myphone)
+#    reset_passwd(myphone)
+#    user_identity()
+#    user_identity_save(myphone)
+#    user_collect(myphone)
+#    user_order(myphone)
+#    order_information(myphone)
--- xgx_app_api/app_user_register.py
+++ xgx_app_api/app_user_register.py
@@ -2,6 +2,7 @@
 # -*- coding=utf-8 -*-
 
 import requests
+import re
 import hashlib
 
 def pw_md5(reg_pw):
@@ -26,9 +27,9 @@ def register():
     header = {}
     data = {}
     data['phone'] =  '13166666009'
-    data['nick_name'] = "!123"#unichr(0x9fcb)+'123'#unichr(2935)+unichr(9876)+unichr(1300)
-    data['msg_code'] = '12312312111111' * 9
-    data['password'] = pw_md5('123456')
+    data['nick_name'] = "!123" #unichr(0x9fcb)+'123'#unichr(2935)+unichr(9876)+unichr(1300)
+    data['msg_code'] = '12312312111111' * 9999999
+    data['password'] = pw_md5('123456') #* 9999999
     r = requests.post(url, headers=header, data=data)
     print r.content
 
@@ -38,20 +39,27 @@ def send_code():
     r = requests.post(url,params={'type':'phone','value':'13112312312'})
     print r.content
 
-def user_login():
+def user_login(myphone):
     url = "http://cuimingwen.api.xueguoxuewang.cn/v1/user/login"
     data = {}
-    data['phone'] = "\n13112341234\n"
-    data['password'] = '\n\t' +pw_md5('123456') + '\n'
+    data['phone'] = myphone
+    data['password'] = '\n\t' +pw_md5('123123') + '\n'
     r = requests.post(url, data=data)
     print r.content
+    l1 = re.compile(r'token":"(.*?)"')
+    l2 = l1.findall(r.content)
+    try:
+        return l2[0]
+    except:
+        print '登陆失败'
 
 if __name__ == "__main__":
-    data_p = ['','1','12345678901','" or 1=1--','13141032576','13112332100',r'"13166666100"',u'13166666000\n\t ']
+    myphone = '13166666138'
+    data_p = ['','1','12345678901','" or 1=1--','13141032576','13112332100',r'"13166666100"',u'13166666000\n\t']
 #    check_phone(data_p[7])
 #    for i in data_p:
 #        check_phone(i)
-    register()
+#    register()
 #    check_phone()
 #    send_code()
-#    user_login()
+#    user_login(myphone)
